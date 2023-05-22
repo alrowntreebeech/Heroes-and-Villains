a20cd98cf43802c08820b7eef651b2485e473f18
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-05-22 16:15:29 +0100

    Create routing and homepage

---
+++ view/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../features/Header"; 
+
+const HomePage = () => {
+    return (
+        <div id="homepage">
+            <Header/>
+        </div>
+    )
+};
+
+export default HomePage;
---
+++ view/src/features/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header id="websiteBanner">
+            <h1> Heroes and Villains - Online Shop </h1>
+        </header>
+    )
+};
+
+export default Header;
