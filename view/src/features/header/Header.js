f5852d4beaefb9041abee3f4f739bf91e288cfdf
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-05-29 14:53:13 +0100

    Tidy code

--- view/src/features/header/Header.js
+++ view/src/features/header/Header.js
@@ -4,7 +4,7 @@ import styles from './header.module.css';
 const Header = () => {
     return (
         <header id={styles.websiteBanner}>
-            <h1> Heroes and Villains - Online Shop </h1>
+            <h1>Heroes and Villains - Online Shop</h1>
         </header>
     )
 };
