8c79c8031e8cb77c1f02a677fc619f3e868b4c3f
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-05-31 09:42:24 +0100

    Add root page and update routing

--- view/src/App.js
+++ view/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Root from './components/root/Root';
 import HomePage from './components/homepage/Homepage';
 import LogInPage from './components/logInPage/LogInPage';
 import SignUpPage from './components/signUpPage/SignUpPage';
 import './App.css';
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={ <HomePage/> }>
+  <Route path='/' element={ <Root/> }>
+    <Route index element={ <HomePage/> } />
     <Route path='/login' element={ <LogInPage/> } />
     <Route path='/register' element={ <SignUpPage/> } />
   </Route>
