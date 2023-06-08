74110b0bbf49bdd7966cfea426eb554180a7fe12
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-06-08 15:48:33 +0100

    Create javascript form files for signup and login

--- view/src/components/logInPage/LogInPage.js
+++ view/src/components/logInPage/LogInPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../features/header/Header";
+import LoginForm from "../../features/logInForm/LogInForm";
 import styles from "./loginPage.module.css";
 
 const LogInPage = () => {
     return (
         <div className={styles.logInPage}> 
             <Header/>
+            <LoginForm/>
         </div>
     )
 }
--- view/src/components/signUpPage/SignUpPage.js
+++ view/src/components/signUpPage/SignUpPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../features/header/Header";
+import SignUpForm from "../../features/signUpForm/SignUpForm";
 import styles from "./signUpPage.module.css";
 
 const SignUpPage = () => {
     return (
         <div className={styles.signUpPage}>
            <Header/>
+           <SignUpForm/>
         </div>
     )
 };
