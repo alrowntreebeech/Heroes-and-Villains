98ae23e221cfc1e16558c706b133f8a9bdad9a21
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-07-10 10:22:13 +0100

    Fix database

--- db/database.js
+++ db/database.js
@@ -8,13 +8,4 @@ const pool = new Pool({
     port: process.env.DB_PORT
 });
 
-module.exports = {
-    query: (text, params, callback) => {
-        const start = Date.now();
-        return pool.query(text, params, (err, res) => {
-            const duration = Date.now() - start;
-            console.log('executed query', { text, duration, rows: res.rowCount })
-            callback(err, res)
-        });
-    },
-};
+module.exports = pool;
